a9b52c1aa44c8bf7e65676781eb55319ae566115
MukeshKG <mukesh1811@gmail.com>
2023-12-09 02:47:09 +0530

    title updated

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'MKG Company - Software Stores',
       home: Scaffold(
         backgroundColor: Colors.amber.shade200,
         body: SafeArea(
